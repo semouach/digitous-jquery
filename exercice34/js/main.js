94c8f45bc035b1557774b91787b58ec070dfbb7b
Semou <semou.ach@gmail.com>
2021-08-03 16:13:33 +0200

    CORRECTION 34

--- exercice34/js/main.js
+++ exercice34/js/main.js
@@ -1,15 +1,33 @@
-$(function () {
-    $(".btn-success").click(function () {
-        $(value).val(function(){
-            
-        })
+// $(function () {
+//     $(".btn-success").click(function () {
+//        const countryName = $("#exercise input.form-control.mb-3").val()
+// ​
+//             const url = 'https://restcountries.eu/rest/v2/name/' + countryName
         
-        $.ajax({
-            url: "https://restcountries.eu/rest/v2/name/usa",
-            success: function (data, statuts, response) {
-                $("#capital").html(data[0].capital);
-                $("#country").html(data[0].name);
-            }
+//         $.ajax({
+//             url: url,
+//             success: function (data, statuts, response) {
+//                 $("#capital").html(data[0].capital);
+//                 $("#country").html(data[0].name);
+//             }
+//         })
+//     })
+// })
+
+$(
+    function () {
+        $(".btn-success").click(function () {
+            const countryName = $("#exercise input.form-control.mb-3").val()
+    
+                const url = 'https://restcountries.eu/rest/v2/name/' + countryName
+
+            $.ajax({
+                url: url,
+                success: function(data, statuts, response) {
+                    $("#country").html(data[0].name)
+                    $("#capital").html(data[0].capital)
+                }
+            })
         })
-    })
-})
+    }
+)
